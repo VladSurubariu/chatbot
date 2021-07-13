3b9a4d912610be541600cdef6c47811a1c235d25
Vlad Surubariu <77098036+VladSurubariu@users.noreply.github.com>
2021-07-13 21:45:37 +0300

    Final 2.0
    Now I hope its really the final version of the project

--- main.cpp
+++ main.cpp
@@ -48,25 +48,18 @@ bool searchFolder(std::string auxNumeFolder){
 void processUserAnswer(){
     std::string userAnswer=readUserAnswer();
     while(userAnswer!="stop") {
-        //std::cout<<"2."<<path<<" "<<std::endl;
         char userSentence[userAnswer.size() + 1];
         strcpy(userSentence, userAnswer.c_str());
         char *wordToken = strtok(userSentence, " ");
         while (wordToken != NULL) {
-            //std::cout<<"3."<<path<<" "<<wordToken<<std::endl;
             std::string word = wordToken;
-            //std::cout<<word<<" "<<wordToken<<" "<<path<<std::endl;
             if (word[word.size() - 1] == '?') {
                 word[word.size() - 1] = '-';
             }
-            //std::cout<<word<<std::endl;
             bool folderCheck = searchFolder(word);
-            //std::cout<<folderCheck<<std::endl;
             if (folderCheck) {
                 changeDirectory(word);
-                //std::cout<<"3"<<path<<" "<<word<<std::endl;
                 if (word[word.size() - 1] == '-' || word[word.size() - 1] == '!' || word[word.size() - 1] == '.') {
-                    //std::cout<<"3b";
                     std::string chatbotAnswer;
                     std::string denumireFisier = word + ".txt";
                     std::ifstream f(denumireFisier);
@@ -75,29 +68,21 @@ void processUserAnswer(){
                 }
             } else {
                 createNewFolder(path, word);
-                //std::cout<<"4."<<path<<" "<<word<<std::endl;
                 changeDirectory(word);
-                //std::cout<<"5."<<path<<" "<<word<<std::endl;
                 if (word[word.size() - 1] == '-' || word[word.size() - 1] == '!' || word[word.size() - 1] == '.') {
                     std::string chatbotAnswer;
                     std::string denumireFisier = word + ".txt";
                     std::ofstream f(denumireFisier);
                     std::cout << "Nu am gasit un raspuns in baza de date. Ofera-mi o sugestie: ";
-                    //std::cin >> chatbotAnswer;
                     std::getline(std::cin, chatbotAnswer);
                     std::cout<<chatbotAnswer<<std::endl;
                     f << chatbotAnswer;
-                    // std::cout<<"6."<<path<<" "<<word<<std::endl;
                 }
             }
-            //std::cout<<"7."<<path<<" "<<word<<std::endl;
             wordToken = strtok(NULL, " ");
             if(wordToken==NULL){
-                //std::cout<<"wordToken==NULL"<<std::endl;
             }
-            //std::cout<<"8."<<path<<" "<<std::endl;
         }
-        //std::cout<<"9";
         userAnswer=readUserAnswer();
         path="D:\\Proiecte\\c++\\Conti Challenge\\Chatbot\\Conversatie";
         _chdir(path.c_str());
