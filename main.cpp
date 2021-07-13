55eab234822ce3ae16669962bacc1748c810fbeb
Vlad Surubariu <77098036+VladSurubariu@users.noreply.github.com>
2021-07-13 21:07:35 +0300

    Final touches
    Finished project

--- main.cpp
+++ main.cpp
@@ -5,15 +5,7 @@
 #include <direct.h>
 #include <cstdlib>
 
-std::string path="D:\\Proiecte\\c++\\Conti Challenge\\Chatbot\\Conversatie";
-
-void myFirstWords(){
-    std::cout<<"Salut. Eu sunt Chatbot. Intreaba-ma orice si voi incerca sa iti raspund"<<std::endl;
-}
-
-void answerNotFound(){
-    std::cout<<"Nu am gasit un raspuns la intrebarea ta. Te rog sa introduci tu raspunsul pe care ar trebui sa ti-l ofer: ";
-}
+std::string path;
 
 std::string readUserAnswer(){
     std::string answer;
@@ -27,25 +19,13 @@ void changeDirectory(std::string toAdd){
     _chdir(path.c_str());
 }
 
-void createNewFolder(std::string auxDirector){
-    std::string newFolderName;
-    std::cout<<"Introduceti numele noului folder: ";
-    std::cin>>newFolderName;
+void createNewFolder(std::string auxDirector, std::string newFolderName){
     auxDirector=auxDirector+"\\";
     auxDirector=auxDirector+newFolderName;
 
     char pathToCreate[auxDirector.size()+1];
     strcpy(pathToCreate, auxDirector.c_str());
-
-    if(_mkdir(pathToCreate)==0){
-        std::cout<<"The folder has been created. The path is: "<<auxDirector;
-    }
-    else{
-        if(errno==2)
-            std::cout<<"No such file or directory."<<std::endl;
-        if(errno==17)
-            std::cout<<"This folder already exists."<<std::endl;
-        }
+    _mkdir(pathToCreate);
 }
 
 
@@ -57,10 +37,6 @@ bool searchFolder(std::string auxNumeFolder){
 
     strcpy(searchFile, auxNumeFolder.c_str());
     error=_searchenv_s(searchFile, envvar, pathBuffer, _MAX_PATH);
-    if (error!=0){
-        std::cout<<"Error number "<<error<<std::endl;
-        return 0;
-    }
     if (*pathBuffer !='\0'){
         return 1;
     }
@@ -70,51 +46,67 @@ bool searchFolder(std::string auxNumeFolder){
 }
 
 void processUserAnswer(){
-    const char questionMark='?';
-    const char exclamantionMark='!';
-    const char fullStop='.';
-    std::string chatbotAnswer;
     std::string userAnswer=readUserAnswer();
-    char userSentence[userAnswer.size()+1];
-    strcpy(userSentence, userAnswer.c_str());
-    char* wordToken=strtok(userSentence, " ");
-    while(wordToken!=NULL){
-        std::cout<<wordToken<<std::endl;
-        bool folderCheck=searchFolder(wordToken);
-        if(folderCheck){
-            std::string word=wordToken;
-            if(word[word.size()-1]==questionMark || word[word.size()-1]==exclamantionMark || word[word.size()-1]==fullStop)
-            {
-                word=word+".txt";
-                std::ifstream f(word);
-                f>>chatbotAnswer;
-                std::cout<<chatbotAnswer;
+    while(userAnswer!="stop") {
+        //std::cout<<"2."<<path<<" "<<std::endl;
+        char userSentence[userAnswer.size() + 1];
+        strcpy(userSentence, userAnswer.c_str());
+        char *wordToken = strtok(userSentence, " ");
+        while (wordToken != NULL) {
+            //std::cout<<"3."<<path<<" "<<wordToken<<std::endl;
+            std::string word = wordToken;
+            //std::cout<<word<<" "<<wordToken<<" "<<path<<std::endl;
+            if (word[word.size() - 1] == '?') {
+                word[word.size() - 1] = '-';
             }
-            else{
-            changeDirectory(wordToken);
+            //std::cout<<word<<std::endl;
+            bool folderCheck = searchFolder(word);
+            //std::cout<<folderCheck<<std::endl;
+            if (folderCheck) {
+                changeDirectory(word);
+                //std::cout<<"3"<<path<<" "<<word<<std::endl;
+                if (word[word.size() - 1] == '-' || word[word.size() - 1] == '!' || word[word.size() - 1] == '.') {
+                    //std::cout<<"3b";
+                    std::string chatbotAnswer;
+                    std::string denumireFisier = word + ".txt";
+                    std::ifstream f(denumireFisier);
+                    while(f >> chatbotAnswer)
+                        std::cout <<" "<< chatbotAnswer;
+                }
+            } else {
+                createNewFolder(path, word);
+                //std::cout<<"4."<<path<<" "<<word<<std::endl;
+                changeDirectory(word);
+                //std::cout<<"5."<<path<<" "<<word<<std::endl;
+                if (word[word.size() - 1] == '-' || word[word.size() - 1] == '!' || word[word.size() - 1] == '.') {
+                    std::string chatbotAnswer;
+                    std::string denumireFisier = word + ".txt";
+                    std::ofstream f(denumireFisier);
+                    std::cout << "Nu am gasit un raspuns in baza de date. Ofera-mi o sugestie: ";
+                    //std::cin >> chatbotAnswer;
+                    std::getline(std::cin, chatbotAnswer);
+                    std::cout<<chatbotAnswer<<std::endl;
+                    f << chatbotAnswer;
+                    // std::cout<<"6."<<path<<" "<<word<<std::endl;
+                }
             }
+            //std::cout<<"7."<<path<<" "<<word<<std::endl;
+            wordToken = strtok(NULL, " ");
+            if(wordToken==NULL){
+                //std::cout<<"wordToken==NULL"<<std::endl;
+            }
+            //std::cout<<"8."<<path<<" "<<std::endl;
         }
-        else{
-            std::cout<<"Folderul trebuie creat"<<std::endl;
-        }
-        wordToken=strtok(NULL, " ");
+        //std::cout<<"9";
+        userAnswer=readUserAnswer();
+        path="D:\\Proiecte\\c++\\Conti Challenge\\Chatbot\\Conversatie";
+        _chdir(path.c_str());
     }
 }
 
 int main(){
-
+    path="D:\\Proiecte\\c++\\Conti Challenge\\Chatbot\\Conversatie";
     _chdir(path.c_str()); //path set
-    //std::string aux="Saluttxt";
-    //std::cout<<searchFolder(aux);
-    /*_chdir(R"(D:\Proiecte\c++\Conti Challenge\Chatbot\Conversatie)");
-    aux="Salut";
-    searchFolder(aux);
-    createNewFolder(R"(D:\Proiecte\c++\Conti Challenge\Chatbot\Conversatie)");
-    */
-    //changeDirectory()
     processUserAnswer();
-
-
-
     return 0;
 }
