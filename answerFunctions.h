a17bfcc75205d9bab3222724763f3801ae97ab13
Vlad Surubariu <77098036+VladSurubariu@users.noreply.github.com>
2021-07-22 13:00:00 +0300

    Update answerFunctions.h

--- answerFunctions.h
+++ answerFunctions.h
@@ -1,27 +1,26 @@
+//header-ul in care sunt stocate functiile folosite pentru a oferi raspuns userului
+
 #pragma once
 
-std::string readUserAnswer(){
+std::string readUserAnswer(){ //functie care citeste propozitia user-ului
     std::string answer;
     std::getline(std::cin, answer);
     return answer;
 }
 
-void checkforMultipleAnswers(){
-
-}
-
-void recordNewAnswer(std::string denumireFisier){
+void recordNewAnswer(std::string denumireFisier){ //functia prin care se inregistreaza raspunsul unei intrebari
+    //ce nu se gaseste in baza de date
     std::string chatbotAnswer;
     std::ofstream createAnswerFile(denumireFisier);
 
     std::cout<<"Nu am gasit un raspuns in baza de date. Ofera-mi o sugestie:";
     std::getline(std::cin, chatbotAnswer);
     createAnswerFile<<chatbotAnswer;
-    std::cout<<"Am inregistrat raspunsul."<<std::endl<<"Doresti sa imi oferi mai multe variante de raspuns? "<<std::endl;
+    std::cout<<"Am inregistrat raspunsul."<<std::endl;
     createAnswerFile.close();
 }
 
-void offerAnswer(std::string denumireFisier){
+void offerAnswer(std::string denumireFisier){ //functia prin care se ofera raspunsul la intrebarea pe care a pus-o userul
     std::string chatbotAnswer;
     std::ifstream answerFile;
     answerFile.open(denumireFisier);
@@ -36,12 +35,13 @@ void offerAnswer(std::string denumireFisier){
     }
 }
 
-std::string tokeniseSentence(char *userSentence){
+std::string tokeniseSentence(char *userSentence){ //functia pentru a tokeniza input-ul oferit de user
     char *wordToken=strtok(userSentence, " ");
     std::string wordFromSentence;
     while(wordToken != NULL){
         wordFromSentence=wordToken;
-        if (wordFromSentence[wordFromSentence.size() - 1] == '?') {
+        if (wordFromSentence[wordFromSentence.size() - 1] == '?') { //daca propozitia oferita drept input are semnul intrebarii
+            //acesta va fi eliminat, deoarece nu se poate folosi "?" in denumirea folderelor
             wordFromSentence[wordFromSentence.size() - 1] = '\0';
         }
         createAnswerFolder(wordFromSentence);
@@ -51,7 +51,7 @@ std::string tokeniseSentence(char *userSentence){
     return wordFromSentence;
 }
 
-void processUserAnswer(){
+void processUserAnswer(){ //functia principala prin care se controleaza actiunile Chatbotului
     std::string userAnswer=readUserAnswer();
     std::string chatbotAnswer;
     while(userAnswer!="stop" && userAnswer!="Stop" && userAnswer!="STOP"){
